afdddc8c837682bdc3d1a185884a12923e5ed134
shahzainhaider <sshnaqvi2001@gmail.com>
2023-10-16 20:51:09 +0500

    initial

---
+++ app.js
@@ -0,0 +1,29 @@
+console.log('connected')
+
+let btns = document.querySelectorAll('.button')
+let display = document.querySelector('input')
+let string = ''
+
+for(let btn of btns){
+    btn.addEventListener('click',(e)=>{
+        if(e.target.innerText == '='){
+            string = eval(string)
+            display.value= string
+        }else if(e.target.innerText == 'X'){
+            string += '*'
+            display.value= string
+        }
+        else if(e.target.innerText == 'C'){
+            string = ''
+            display.value= string
+        }
+        else{
+            string += e.target.innerText
+            display.value= string
+            }
+
+
+        // console.log(e.target.innerText)
+
+    })
+}
