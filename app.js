11129f8e2d7f27f35e90f1d5d80a94ba2d808756
shahzainhaider <sshnaqvi2001@gmail.com>
2023-10-19 16:33:09 +0500

    repeated op fix

--- app.js
+++ app.js
@@ -2,24 +2,33 @@ console.log('connected')
 
 let btns = document.querySelectorAll('.button')
 let display = document.querySelector('input')
+display.value = 0
 let string = ''
 
-for(let btn of btns){
-    btn.addEventListener('click',(e)=>{
-        if(e.target.innerText == '='){
-            string = eval(string)
-            display.value= string
-        }else if(e.target.innerText == 'X'){
-            string += '*'
-            display.value= string
-        }
-        else if(e.target.innerText == 'C'){
-            string = ''
-            display.value= string
-        }
-        else{
-            string += e.target.innerText
-            display.value= string
+
+Array.from(btns).forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+        try {
+            if ('+-*X/'.includes(display.value[display.value.length - 1]) && '+-X*/'.includes(e.target.innerText)) {
+                string = string.slice(0, -1)
             }
+            if (e.target.innerText == '=') {
+                string = eval(string)
+                display.value = string
+            } else if (e.target.innerText == 'X') {
+                string += '*'
+                display.value = string
+            }
+            else if (e.target.innerText == 'C') {
+                string = ''
+                display.value = 0
+            }
+            else {
+                string += e.target.innerText
+                display.value = string
+            }
+        } catch (error) {
+            display.value = 'Invalid expression'
+        }
     })
-}
+})
